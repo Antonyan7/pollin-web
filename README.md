5ca42bbe8cd64a4d3d6468bd79d5aa43fb54dfed
Harpreet Gill <71853906+harpreet-gill-shopthing@users.noreply.github.com>
2022-08-19 17:45:55 -0400

    [PCP-265] Added Support for Danger Validation on Pull Requests (#49)
    * PCP-265: Added Support for Danger Validation on Pull Requests
    
    * PCP-265: Added Danger to dependencies
    
    * PCP-265: Fixed issue with dependency for babel
    
    * PCP-265: PCP-265: Disabled Danger Transpilation
    
    * PCP-265: Fixed Lint Issues
    
    Co-authored-by: Harpreet Gill <>

--- README.md
+++ README.md
@@ -39,3 +39,9 @@ Prettier - Prettier is an opinionated code formatter.
 Eslint - ESLint is an open source project that helps you find and fix problems with your JavaScript code.
 
 Redux Toolkit (Thunk) - For state management is used Redux Thunk with Toolkit. It's a middleware that lets you call action creators that return a function instead of an action object.
+
+
+# Documentation
+
+1. [Development Polcies](https://docs.google.com/document/d/1Iez6mYaCN5FO3Ehfb8MgCPe1MiSL_LRpLI5lj_3VFDg/edit#heading=h.e77667m46viu)
+1. [API Rules](https://github.com/OPN-Technologies/pollin-tech-docs/blob/master/APIRequirements/GeneralAPIRules.md)
---
+++ dangerfile.ts
@@ -0,0 +1,58 @@
+import { danger, fail, markdown, warn } from 'danger';
+
+// Setup
+const { github } = danger;
+const { pr } = github;
+// const commits = github.commits;
+const modified = danger.git.modified_files;
+// console.log(commits.map(({ sha }) => sha));
+const noTicketNumber = pr.title.match(/^(\[NO_TICKET])\s\w+/);
+const dependabotRequest = pr.title.match(/(.*)(bump)/);
+
+let errorCount = 0;
+const bigPRThreshold = 500;
+
+// Custom subsets of known files
+const modifiedAppFiles = modified.filter((filePath) => filePath.includes('src/') || filePath.includes('test/'));
+// Check test exclusion (.only) is included
+const modifiedSpecFiles = modified.filter((filePath) => filePath.match(/-spec.(js|jsx|ts|tsx)$/gi));
+
+if (!noTicketNumber && !dependabotRequest) {
+  if (!pr.title.match(/^(?:(\[PPM-\d+])|(\[PCP-\d+]))\s\w+/)) {
+    fail('PR title should include JIRA ticket number [PPM/PCP-{number}] {description}.');
+  }
+
+  if (!pr.body.match(/(.*)(?:(\[PPM-\d+])|(\[PCP-\d+]))/) && !noTicketNumber) {
+    fail('PR body should include JIRA ticket number [PPM/PCP-{number}]');
+  }
+
+  if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+    warn(`:exclamation: Big PR (${(errorCount += 1)})`);
+    markdown(
+      `> (${errorCount}) : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.`
+    );
+  }
+
+  const packageChanged = modified.includes('package.json');
+  const lockfileChanged = modified.includes('yarn.lock');
+
+  if (packageChanged && !lockfileChanged) {
+    warn(`:exclamation: yarn.lock not updated (${(errorCount += 1)})`);
+
+    const message = 'Changes were made to package.json, but not to yarn.lock';
+    const idea = 'Perhaps you need to run `yarn install`?';
+
+    warn(`${message} - <i>${idea}</i>`);
+    markdown(`> (${errorCount}) : ${message} - <i>${idea}</i>`);
+  }
+
+  const hasAppChanges = modifiedAppFiles.length > 0;
+  const hasTestChanges = modifiedSpecFiles.length > 0;
+
+  if (hasAppChanges && !hasTestChanges) {
+    warn(`:exclamation: Missing Tests (${(errorCount += 1)})`);
+    markdown(
+      `> (${errorCount}) : there are app changes, but not tests. That's OK as long as you're refactoring existing code`
+    );
+  }
+}
