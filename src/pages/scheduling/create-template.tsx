768da1022c25bb7291a6dae82f1dab0538297903
Arthur Voskanyan <96148671+Arthur02V@users.noreply.github.com>
2022-08-23 07:34:33 +0400

    FIX: add header for schedule template (#62)

--- src/pages/scheduling/create-template.tsx
+++ src/pages/scheduling/create-template.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import MainBreadcrumb from '@components/Breadcrumb/MainBreadcrumb';
 import CreateSchedulingTemplateStyled from '@components/Scheduling/createSchedulingStyled';
 import CreateTemplate from '@components/Scheduling/createTemplate';
 
 const CreateSchedulingTemplate = () => (
   <CreateSchedulingTemplateStyled>
     <div className="create-scheduling-template">
+      <MainBreadcrumb
+        currentPage="Create Schedule Templates"
+        navigation={{
+          basePath: '/',
+          items: [{ name: 'Create Schedule Templates', path: '/scheduling/create-template' }]
+        }}
+      />
       <CreateTemplate />
     </div>
   </CreateSchedulingTemplateStyled>
