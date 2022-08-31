ba5b4069ff31c4595e806573119541353481813a
Karen <109095129+KarenBoyakhchyan@users.noreply.github.com>
2022-09-01 02:46:40 +0400

    feat:  Implement header navigation for Blocked scheduled (#88)
    Co-authored-by: karen-boyakhchyan <karen.boyakhchyan@prt.manomano.com>

--- src/pages/scheduling/block-schedule.tsx
+++ src/pages/scheduling/block-schedule.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import MainBreadcrumb from '@components/Breadcrumb/MainBreadcrumb';
 import BlockScheduleComponent from '@components/Scheduling/blockSchedule';
+import { Box } from '@mui/material';
 
-const BlockSchedule = () => <BlockScheduleComponent />;
+const BlockSchedule = () => (
+  <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+    <MainBreadcrumb
+      currentPage="Block Schedule"
+      navigation={{
+        basePath: '/',
+        items: [{ name: 'Block Schedule', path: '/scheduling/block-schedule' }]
+      }}
+    />
+    <BlockScheduleComponent />
+  </Box>
+);
 
 export default BlockSchedule;
