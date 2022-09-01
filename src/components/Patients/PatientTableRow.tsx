15ac1c8a8c50b9318c7f57a1c0d42c803953a1bb
Arthur Voskanyan <96148671+Arthur02V@users.noreply.github.com>
2022-09-02 01:45:52 +0400

    FEAT: redirect to patient details page (#99)

--- src/components/Patients/PatientTableRow.tsx
+++ src/components/Patients/PatientTableRow.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Badge, TableCell, TableRow, Typography } from '@mui/material';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import Chip from '@ui-component/patient/Chip';
 
@@ -15,13 +16,25 @@ interface IPatientTableRow {
 
 const PatientTableRow = ({ row, index }: IPatientTableRow) => {
   const labelId = `enhanced-table-checkbox-${index}`;
+  const router = useRouter();
+
+  const onRowClick = (id: string) => {
+    router.push({ pathname: '/patient-emr/patient-details', query: { patientId: id } });
+  };
 
   return (
-    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+    <TableRow
+      sx={{ cursor: 'pointer' }}
+      onClick={() => onRowClick(row.id)}
+      hover
+      role="checkbox"
+      tabIndex={-1}
+      key={row.id}
+    >
       <TableCell component="th" id={labelId} scope="row" sx={{ cursor: 'pointer' }}>
         <Image src={avatarIconPatient} />
       </TableCell>
-      <TableCell component="th" id={labelId} scope="row" sx={{ cursor: 'pointer' }}>
+      <TableCell component="th" id={labelId} scope="row">
         <Typography variant="subtitle1" sx={{ color: 'grey.900' }}>
           {row.title}
         </Typography>
---
+++ src/pages/patient-emr/patient-details.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+// TODO : add logic here with PCP-277
+const PatientDetails = () => <Box />;
+
+export default PatientDetails;
