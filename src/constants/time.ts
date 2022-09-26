5d813cca1b97fe6b936af4b972e1cca3a67d5d36
Yervand <112645576+yervand13m@users.noreply.github.com>
2022-09-26 19:10:15 +0400

    [PCP-587] Add appointment modal date/time picker is broken (#189)
    ### What are you trying to accomplish?
    [PCP-587]
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    MobileDateTimePicker, and any timePicker
    
    [PCP-587]:
    https://fhhealth.atlassian.net/browse/PCP-587?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/constants/time.ts
+++ src/constants/time.ts
@@ -1,2 +1,2 @@
-export const MAX_SELECTABLE_DATE_TIME = new Date(1970, 1, 1, 7, 0, 0);
-export const MIN_SELECTABLE_DATE_TIME = new Date(1970, 1, 1, 18, 0, 0);
+export const MAX_SELECTABLE_DATE_TIME = new Date(1970, 1, 1, 18, 0, 0, 0);
+export const MIN_SELECTABLE_DATE_TIME = new Date(1970, 1, 1, 7, 0, 0, 0);
