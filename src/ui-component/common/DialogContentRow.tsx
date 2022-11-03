d63c80ebf87e59dd5f66497a8ff069b4139afd4c
Karen <109095129+KarenBoyakhchyan@users.noreply.github.com>
2022-11-03 21:59:18 +0400

    [PCP-977] Label name should be in Bold as per the given design (#397)
    ### What are you trying to accomplish?
    
    [PCP-977](https://fhhealth.atlassian.net/browse/PCP-977)
    
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    
    <img width="834" alt="Screen Shot 2022-11-03 at 4 05 35 PM"
    src="https://user-images.githubusercontent.com/109095129/199716201-f1e529e0-0bce-4838-bc78-f43878361f90.png">
    
    Co-authored-by: karen-boyakhchyan <karen.boyakhchyan@prt.manomano.com>

--- src/ui-component/common/DialogContentRow.tsx
+++ src/ui-component/common/DialogContentRow.tsx
@@ -9,7 +9,9 @@ interface DialogContentRowProps {
 const DialogContentRow = ({ subtitle, body }: DialogContentRowProps) => (
   <Grid item xs={12}>
     <Stack direction="row" spacing={1} alignItems="center">
-      <Typography variant="subtitle1">{subtitle}</Typography>
+      <Typography variant="subtitle1" fontWeight="bold">
+        {subtitle}
+      </Typography>
       <Typography variant="body2">{body}</Typography>
     </Stack>
   </Grid>
