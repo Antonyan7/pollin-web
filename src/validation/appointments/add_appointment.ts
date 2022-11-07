7eed17eb188ca43dfdbac23035c6128dd92a41af
Karen <109095129+KarenBoyakhchyan@users.noreply.github.com>
2022-11-07 19:12:49 +0400

    [PCP-940] Show the Success message and validation message on the Calendar screen (#416)
    ### What are you trying to accomplish?
    
    [PCP-940](https://fhhealth.atlassian.net/browse/PCP-940)
    
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    
    <img width="641" alt="Screen Shot 2022-11-07 at 11 19 23 AM"
    src="https://user-images.githubusercontent.com/109095129/200248190-46dd2547-2c89-4746-9cae-a649ba2641e7.png">
    
    Co-authored-by: karen-boyakhchyan <karen.boyakhchyan@prt.manomano.com>

--- src/validation/appointments/add_appointment.ts
+++ src/validation/appointments/add_appointment.ts
@@ -2,6 +2,8 @@ import { AutocompleteInputChangeReason } from '@mui/material';
 import { generateErrorMessage } from 'helpers/generateErrorMessage';
 import { date, object, string } from 'yup';
 
+import { futureDate180DaysAfter } from '@utils/dateUtils';
+
 export const validateInputChange = (_: React.SyntheticEvent, value: string, reason: AutocompleteInputChangeReason) => {
   if (reason === 'reset') {
     return '';
@@ -13,6 +15,9 @@ export const validateInputChange = (_: React.SyntheticEvent, value: string, reas
 export const addAppointmentsValidationSchema = object({
   serviceTypeId: string().required(generateErrorMessage('Appointment type')),
   patientId: string().required(generateErrorMessage('Patient')),
-  description: string().notRequired().max(250),
-  date: date().required(generateErrorMessage('Date'))
+  description: string().notRequired().max(250, 'Description is too much longer'),
+  date: date()
+    .max(futureDate180DaysAfter, "Can't select days for future more than 180 days")
+    .required(generateErrorMessage('Date'))
+    .nullable(true)
 });
