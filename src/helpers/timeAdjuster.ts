311afc887389cea4652625d251e31a9066568be3
Karen <109095129+KarenBoyakhchyan@users.noreply.github.com>
2022-12-07 17:02:11 +0400

    PCP-1458: User is not able to see 24hr format on Appointment details modal for Past Appointment (#559)
    ### What are you trying to accomplish?
    
    https://fhhealth.atlassian.net/browse/PCP-1458
    
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    
    
    <img width="843" alt="Screen Shot 2022-12-07 at 01 29 02"
    src="https://user-images.githubusercontent.com/109095129/206027019-2f2f83fe-49ed-4183-b9e2-8c1926852d70.png">

--- src/helpers/timeAdjuster.ts
+++ src/helpers/timeAdjuster.ts
@@ -4,7 +4,11 @@ export const timeAdjuster = (date: Date | string) => {
   const customizedDate = `${monthNames[new Date(date).getMonth()]} ${new Date(date).getDate()}, ${new Date(
     date
   ).getFullYear()}`;
-  const customizedTime = new Date(date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const customizedTime = new Date(date).toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  });
   const customizedFullDate = `${customizedDate} ${customizedTime} [EST]`;
 
   return {
