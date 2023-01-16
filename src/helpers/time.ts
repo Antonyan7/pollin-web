77029256ae6f5f31db866fd9c0c98fc5e091ad57
Yervand <112645576+yervand13m@users.noreply.github.com>
2023-01-16 16:27:10 +0400

    PCP-2005: Fix User can select passed working hours (#710)
    ### What are you trying to accomplish?
    [PCP-2005]
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing
    
    
    https://user-images.githubusercontent.com/112645576/212483503-f1e2675b-d525-417c-84cc-eb77dffa46a5.mp4
    
    
    
    
    [PCP-2005]:
    https://fhhealth.atlassian.net/browse/PCP-2005?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -3,8 +3,24 @@ import { format } from 'date-fns';
 
 import { TIME_CONFIG } from './constants';
 
-export const toRoundupTime = (time: Date | string | null) =>
-  time ? new Date(Math.floor(new Date(time).getTime() / roundUpTo) * roundUpTo) : null;
+export const toRoundupTime = (selectedTime: Date | string | null) => {
+  if (selectedTime) {
+    const dateTime = new Date(selectedTime);
+    const minutes = dateTime.getMinutes();
+    const hours = dateTime.getHours();
+
+    // ? When selected time is greater than maximum available time, then set maximum time value for as a rounding up time to avoid select working hours which are passed.
+    if (hours >= 18 && minutes > 0) {
+      dateTime.setMinutes(0);
+    }
+
+    const roundedTimeValue = Math.floor(dateTime.getTime() / roundUpTo);
+
+    return new Date(roundedTimeValue * roundUpTo);
+  }
+
+  return null;
+};
 
 export const formatDate = (date: Date | null) =>
   date ? format(new Date(date), `MMM dd, yyy hh:mm a '[${TIME_CONFIG || ''}]'`) : '';
