0d34de95deab0cc9347f5a4ee75b04146e43251b
Sergei Antonyan <39464948+Antonyan7@users.noreply.github.com>
2023-02-01 00:14:00 +0400

    PCP-2101: orders reset on create (#802)
    ### What are you trying to accomplish?
    [PCP-2101]
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    Open Create order page select some dropdowns, go back to another patient
    and you will see reseted dropdowns
    
    
    [PCP-2101]:
    https://fhhealth.atlassian.net/browse/PCP-2101?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/components/Orders/CreateOrder/index.tsx
+++ src/components/Orders/CreateOrder/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CreateOrderActions from '@components/Orders/CreateOrder/layout/CreateOrderActions';
 import CreateOrderBody from '@components/Orders/CreateOrder/layout/CreateOrderBody';
 import CreateOrderHeader from '@components/Orders/CreateOrder/layout/CreateOrderHeader';
 import { Stack } from '@mui/material';
+import { dispatch } from '@redux/hooks';
+import { ordersMiddleware } from '@redux/slices/orders';
 
 import { OrderCreationContext } from '../../../context/OrderCreationContext';
 
-const CreateOrder = () => (
-  <Stack>
-    <OrderCreationContext>
-      <CreateOrderHeader />
-      <CreateOrderBody />
-      <CreateOrderActions />
-    </OrderCreationContext>
-  </Stack>
-);
+const CreateOrder = () => {
+  useEffect(() => {
+    dispatch(ordersMiddleware.resetOrderGroupsSelection());
+  }, []);
+
+  return (
+    <Stack>
+      <OrderCreationContext>
+        <CreateOrderHeader />
+        <CreateOrderBody />
+        <CreateOrderActions />
+      </OrderCreationContext>
+    </Stack>
+  );
+};
 
 export default CreateOrder;
