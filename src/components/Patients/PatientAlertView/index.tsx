ffb63c53531c48052f6a82e9b24c6de070f861ca
Sirusho Martikyan <74008883+Siirusho@users.noreply.github.com>
2023-03-13 18:16:52 +0400

    PCP-2882: Added hover functionality for custom alerts in Patient Details page (#1002)
    ### What are you trying to accomplish?
    [PCP-2882](https://fhhealth.atlassian.net/browse/PCP-2882)
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    <img width="724" alt="Screenshot 2023-03-12 at 18 55 39"
    src="https://user-images.githubusercontent.com/74008883/224553327-73049264-438e-4436-b353-903a5ee77c30.png">
    
    
    [PCP-2882]:
    https://fhhealth.atlassian.net/browse/PCP-2882?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: Sirusho Martikyan <sirusho.martikyan@burberry.com>

--- src/components/Patients/PatientAlertView/index.tsx
+++ src/components/Patients/PatientAlertView/index.tsx
@@ -3,15 +3,19 @@ import { useTranslation } from 'react-i18next';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
-import { Button, Grid, Stack, Typography, useTheme } from '@mui/material';
+import { Button, Grid, Stack, Tooltip, tooltipClasses, TooltipProps, Typography, useTheme } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { viewsMiddleware } from '@redux/slices/views';
 import { Translation } from 'constants/translations';
+import { format } from 'date-fns';
 import { useRouter } from 'next/router';
 import { dispatch, useAppSelector } from 'redux/hooks';
 import { patientsMiddleware, patientsSelector } from 'redux/slices/patients';
 import { borderRadius, margins, paddings } from 'themes/themeConstants';
 import { ModalName } from 'types/modals';
 import { AlertDetailsMessagesProps, AlertDetailsProps } from 'types/reduxTypes/patient-emrStateTypes';
+
+import { convertToLocale } from '@utils/dateUtils';
 
 const PatientAlertView = () => {
   const router = useRouter();
@@ -27,6 +31,16 @@ const PatientAlertView = () => {
     }
   }, [rowId]);
 
+  const MuiTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(() => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.palette.primary.main,
+      fontSize: theme.typography.pxToRem(12),
+      padding: paddings.all12
+    }
+  }));
+
   return (
     <Stack
       width="100%"
@@ -40,53 +54,67 @@ const PatientAlertView = () => {
         <>
           {patientAlertDetails.map((titleContent: AlertDetailsProps, index) => (
             <Grid container>
-              <Grid item container alignItems="center" justifyContent="center" xs={0.5}>
-                <ReportGmailerrorredIcon sx={{ color: theme.palette.warning.dark }} />
-              </Grid>
-              <Grid item xs={10.5}>
-                <Typography fontWeight="600" variant="subtitle1">
-                  {titleContent.title}
-                </Typography>
-                {titleContent.messages.map((message: AlertDetailsMessagesProps) => (
-                  <Typography variant="subtitle2" color={theme.palette.common.black} marginBottom={margins.bottom8}>
-                    {message.title}
-                  </Typography>
-                ))}
-              </Grid>
-              <Grid item xs={0.5} container alignItems="center" justifyContent="center">
-                {patientAlertDetails[index].isEditable ? (
-                  <Button
-                    sx={{ color: theme.palette.warning.dark, mr: '30px' }}
-                    variant="text"
-                    onClick={() => {
-                      dispatch(
-                        viewsMiddleware.openModal({
-                          name: ModalName.AddOrEditCustomAlertModal,
-                          props: {
-                            alertId: titleContent.id,
-                            title: titleContent.title,
-                            description: titleContent.messages[0].title
-                          }
-                        })
-                      );
-                    }}
-                  >
-                    <Typography variant="subtitle2" color={theme.palette.warning.dark}>
-                      {t(Translation.COMMON_BUTTON_EDIT_LABEL)}
+              <MuiTooltip
+                followCursor
+                title={
+                  titleContent.id ? (
+                    <>
+                      {titleContent?.createdBy?.name} -{' '}
+                      {`${format(new Date(convertToLocale(titleContent?.createdBy?.date)), 'MMM dd, yyyy HH:mm')} EST`}
+                    </>
+                  ) : null
+                }
+              >
+                <Grid item container alignItems="center" justifyContent="center">
+                  <Grid item xs={0.5}>
+                    <ReportGmailerrorredIcon sx={{ color: theme.palette.warning.dark }} />
+                  </Grid>
+                  <Grid item xs={10.5}>
+                    <Typography fontWeight="600" variant="subtitle1">
+                      {titleContent.title}
                     </Typography>
-                  </Button>
-                ) : null}
-              </Grid>
-              <Grid item xs={0.5} container alignItems="center" justifyContent="center">
-                {index === 0 ? (
-                  <Button
-                    sx={{ color: theme.palette.warning.dark }}
-                    onClick={() => dispatch(patientsMiddleware.isPatientAlertViewOpen(false))}
-                  >
-                    <CloseIcon sx={{ fontSize: theme.typography.pxToRem(20) }} />
-                  </Button>
-                ) : null}
-              </Grid>
+                    {titleContent.messages.map((message: AlertDetailsMessagesProps) => (
+                      <Typography variant="subtitle2" color={theme.palette.common.black} marginBottom={margins.bottom8}>
+                        {message.title}
+                      </Typography>
+                    ))}
+                  </Grid>
+                  <Grid item xs={0.5} container alignItems="center" justifyContent="center">
+                    {patientAlertDetails[index].isEditable ? (
+                      <Button
+                        sx={{ color: theme.palette.warning.dark, mr: margins.right32 }}
+                        variant="text"
+                        onClick={() => {
+                          dispatch(
+                            viewsMiddleware.openModal({
+                              name: ModalName.AddOrEditCustomAlertModal,
+                              props: {
+                                alertId: titleContent.id,
+                                title: titleContent.title,
+                                description: titleContent.messages[0].title
+                              }
+                            })
+                          );
+                        }}
+                      >
+                        <Typography variant="subtitle2" color={theme.palette.warning.dark}>
+                          {t(Translation.COMMON_BUTTON_EDIT_LABEL)}
+                        </Typography>
+                      </Button>
+                    ) : null}
+                  </Grid>
+                  <Grid item xs={0.5} container alignItems="center" justifyContent="center">
+                    {index === 0 ? (
+                      <Button
+                        sx={{ color: theme.palette.warning.dark }}
+                        onClick={() => dispatch(patientsMiddleware.isPatientAlertViewOpen(false))}
+                      >
+                        <CloseIcon sx={{ fontSize: theme.typography.pxToRem(20) }} />
+                      </Button>
+                    ) : null}
+                  </Grid>
+                </Grid>
+              </MuiTooltip>
             </Grid>
           ))}
         </>
