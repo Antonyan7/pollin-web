c39dd65dcee5cfcb79f2916d010e7169ebc1c1cd
Karen <109095129+KarenBoyakhchyan@users.noreply.github.com>
2023-04-20 21:42:15 +0400

    TEAMA-5420: User isn't able to type in patient field while booking an appointment (#1239)
    ### What are you trying to accomplish?
    
    https://fhhealth.atlassian.net/browse/TEAMA-5420
    
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    
    
    
    https://user-images.githubusercontent.com/109095129/233439845-7b60f7db-848b-4a50-85b7-a2fbf32fc18e.mov

--- src/ui-component/BaseDropdownWithLoading/index.tsx
+++ src/ui-component/BaseDropdownWithLoading/index.tsx
@@ -121,7 +121,6 @@ const BaseDropdownWithLoading = <
                 color: theme.palette.primary.dark
               }
             }}
-            onKeyDown={(e) => e.preventDefault()}
             InputProps={{
               ...params.InputProps,
               endAdornment:
