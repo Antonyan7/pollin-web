e236f69e5d240aba2f23cdaa6156aa13ae4271e7
Sergei Antonyan <39464948+Antonyan7@users.noreply.github.com>
2023-04-27 21:41:05 +0400

    TEAMA-5524: Fix sorting issue for Service Types (#1281)
    ### What are you trying to accomplish?
    [TEAMA-5524]
    
    ### What approach did you take and why?
    
    ### Elaborate any architectural decisions made
    
    ### What should reviewers focus on?
    
    ### Testing?
    
    ![image](https://user-images.githubusercontent.com/39464948/234935341-7c1652db-7e95-46a3-bd69-b9d8c9deec8b.png)
    
    ![image](https://user-images.githubusercontent.com/39464948/234935377-ab505bcf-968b-4712-b777-de53dc3cf451.png)
    
    
    [TEAMA-5524]:
    https://fhhealth.atlassian.net/browse/TEAMA-5524?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/utils/sortUtils.ts
+++ src/utils/sortUtils.ts
@@ -1,12 +1,13 @@
 import { IServiceType } from 'types/reduxTypes/bookingStateTypes';
 
+// Sorting by title and checking if first letter is number
 export const sortServiceTypesByAlphabeticOrder = (arrayForSort: IServiceType[]) => {
   arrayForSort.sort((a: IServiceType, b: IServiceType) => {
-    if (a.title < b.title) {
+    if (!a.title?.[0].match(/[0-9]/) && a.title < b.title) {
       return -1;
     }
 
-    if (a.title > b.title) {
+    if (a.title?.[0].match(/[0-9]/) && a.title > b.title) {
       return 1;
     }
 
